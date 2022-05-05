9eea7fb9e8daf6f837feb0007e7f441a2bd87160
lylyzyta <lylyzyta@gmail.com>
2022-05-05 12:55:48 -0500

    API completa

--- mdLinks/bin/index.js
+++ mdLinks/bin/index.js
@@ -1,20 +1,30 @@
 #!/usr/bin/env node
 
 const mdLinks = require('../../components/mdLinks.js');
-const yargs = require("yargs");
-console.log(process.argv[2])
+//onst yargs = require("yargs");
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
+//console.log(argv)
+
+/* 
+if (argv.validate) {
+  console.log('Plunder more riffiwobbles!')
+} else {
+  console.log('Retreat from the xupptumblers!')
+} */
 
  if(!yargs.argv.validate && !yargs.argv.stats){
-  mdLinks(process.argv[2], false, false)
+ mdLinks()
   .then(function (mensaje) {
     console.log(mensaje);
-    //console.log("soy stats y validate false");
   })
   .catch(function (error) {
     console.log(error);
   });
 
-}else if(yargs.argv.validate && !yargs.argv.stats){
+
+/* }else if(yargs.argv.validate && !yargs.argv.stats){
   mdLinks(process.argv[2], true, false)
   .then(function (mensaje) {
     console.log(mensaje);
@@ -38,10 +48,10 @@ console.log(process.argv[2])
   mdLinks(process.argv[2], true, true )
   .then(function (mensaje) {
     console.log(mensaje);
-    //console.log("soy validate y stats true ")
+    console.log("soy validate y stats true ")
     
   })
   .catch(function (error) {
     console.log(error);
-  });
-} 
+  }); */
+
